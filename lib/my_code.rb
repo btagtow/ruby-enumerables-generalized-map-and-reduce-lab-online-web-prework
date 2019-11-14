e5a52f884524fe298b45da56922473e7c1f548e7
Ben Tagtow <bentagtow@gmail.com>
2019-11-14 01:26:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map(source_array)
   new_array
 end 
 
-def reduce(source_array, starting_value = 0)
+def reduce(source_array, starting_value*)
   new_value = 0
   index = 0 
   while index < source_array.length do 
